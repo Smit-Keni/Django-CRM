2396263e7f26708df5ca774ef40892b020cfc04a
Smit-Keni <smitkeni45@gmail.com>
2024-12-30 14:56:33 +0530

    view records

--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
 
     if request.method=="POST":
         username = request.POST['username']
@@ -21,7 +23,7 @@ def home(request):
             messages.success(request,"There was an Error logging in.")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
     
     
 
@@ -48,3 +50,11 @@ def register_user(request):
         form = SignUpForm()
         return render(request,'register.html',{'form':form})
     return render(request,'register.html',{'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"Access Denied.")
+        return redirect('home')
